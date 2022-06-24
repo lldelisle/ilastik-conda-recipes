9d21b8204ffbcccf4807a9a9b7f08e91f145dd8d
Lucille Delisle <lucille.delisle@epfl.ch>
2022-06-24 15:42:39 +0200

    launch javascript before Fiji

--- recipes/fiji-ilastik/build.sh
+++ recipes/fiji-ilastik/build.sh
@@ -1,6 +1,13 @@
 #! /bin/bash
 
 # Copy the plugin in the good directory:
-outdir=$PREFIX/share/plugins/
+outdir="$PREFIX/share/plugins/"
 mkdir -p $outdir
 cp *.jar $outdir/
+
+# Replace ImageJ by a bash script:
+mv "$PREFIX/bin/ImageJ" "$PREFIX/bin/ImageJ_before_set_exec_ilastik"
+cp "${RECIPE_DIR}/ImageJ" "${PREFIX}/bin/ImageJ"
+cp "${RECIPE_DIR}/IlastikSetter.java" "${PREFIX}/IlastikSetter.java"
+
+chmod a+x "${PREFIX}/bin/ImageJ"
